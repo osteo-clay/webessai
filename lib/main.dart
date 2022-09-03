461aaadbcdaad7a4afce5495c086843e1b02a293
osteo-clay <osteo.clay@gmail.com>
2022-09-03 21:07:28 +0200

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -126,7 +126,7 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Welcome!', style: Theme.of(context).textTheme.headline2),
+        child: Text('Welcome Back!', style: Theme.of(context).textTheme.headline2),
       ),
     );
   }
